f05ddfecc37ab5fbf9466b9da1172515d83ee838
MeloMtom <melomtom@gmail.com>
2017-03-06 21:48:16 +0200

    Create Ex4Q1_Vrot.py

---
+++ Ex4Q1_Vrot.py
@@ -0,0 +1,6 @@
+#---------------------------------------------------
+#NASSP Honours - CM
+#Author: Melukuhle Mtombeni MTMMEL002
+#Date: 
+#Exercise 4 - Question 1 - Plotting The rotational velocity parameterisation
+#--------------------------------------------------------------
